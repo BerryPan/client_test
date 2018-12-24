78ade90eda82be597a56e0ba97761aa9d068f3db
BerryPan <313513799@qq.com>
2018-12-25 00:49:48 +0800

    Add project files.

---
+++ Assets/client.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Linq;
+using Google.Protobuf;
+
+public class client : MonoBehaviour
+{
+    void Start()
+    {
+        ConncetServer();
+    }
+
+
+    void ConncetServer()
+    {
+        IPAddress ipAdr = IPAddress.Parse("127.0.0.1");
+        IPEndPoint ipEp = new IPEndPoint(ipAdr, 1234);
+        Socket clientScoket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        clientScoket.Connect(ipEp);
+        for(int i = 0; i < 100; ++i)
+        {
+            Local loc = new Local { PosX = 12, PosY = 12, PosZ = 32, Name = "abc", RotX = 12, RotY = 123, RotZ = 34};
+            byte[] concent = loc.ToByteArray();
+            clientScoket.Send(concent);
+
+            byte[] response = new byte[1024];
+            int len_recv = clientScoket.Receive(response);
+            byte[] data = response.Take(len_recv).ToArray();
+            Another another = Another.Parser.ParseFrom(data);
+            GameObject.Find("abc").GetComponent<Transform>().position = new Vector3(another.PosX, another.PosY, another.PosZ);
+            GameObject.Find("abc").GetComponent<Transform>().rotation = Quaternion.Euler(another.RotX, another.RotY, another.RotZ);
+            len_recv = clientScoket.Receive(response);
+
+        }
+        
+
+        clientScoket.Shutdown(SocketShutdown.Both);
+        clientScoket.Close();
+    }
+}
---
+++ Assets/local.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Linq;
+using Google.Protobuf;
+
+
+public class local : MonoBehaviour
+{
+    void Start()
+    {
+        ConncetServer_local();
+    }
+
+
+    void ConncetServer_local()
+    {
+        IPAddress ipAdr = IPAddress.Parse("127.0.0.1");
+        IPEndPoint ipEp = new IPEndPoint(ipAdr, 1234);
+        Socket clientScoket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        clientScoket.Connect(ipEp);
+        for (int i = 0; i < 100; ++i)
+        {
+            float xx = GameObject.Find("def").GetComponent<Transform>().position.x;
+            float yy = GameObject.Find("def").GetComponent<Transform>().position.y;
+            float zz = GameObject.Find("def").GetComponent<Transform>().position.z;
+            float rx = GameObject.Find("abc").GetComponent<Transform>().rotation.x;
+            float ry = GameObject.Find("abc").GetComponent<Transform>().rotation.y;
+            float rz = GameObject.Find("abc").GetComponent<Transform>().rotation.z;
+            Local loc = new Local{ PosX = xx, PosY = yy, PosZ = zz , Name = "def"};
+            byte[] concent = loc.ToByteArray();
+            clientScoket.Send(concent);
+            byte[] response = new byte[1024];
+            int len_recv = clientScoket.Receive(response);
+            byte[] data = response.Take(len_recv).ToArray();
+            Another another = Another.Parser.ParseFrom(data);
+            print(another);
+            len_recv = clientScoket.Receive(response);
+        }
+
+
+        clientScoket.Shutdown(SocketShutdown.Both);
+        clientScoket.Close();
+    }
+}
